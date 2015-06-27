3bd337abdad4577de3341f80cb2d6a116585aa13
ajaynbiswas <ajayn.biswas@gmail.com>
2015-06-27 16:13:23 +0530

    Changes for Rooms table

---
+++ Database/2015/June/00005_Remove_underscore_from_column_in_Rooms_table.sql
@@ -0,0 +1,51 @@
+USE [LYSAdmin]
+GO
+
+ALTER TABLE [dbo].[Rooms] DROP CONSTRAINT [FK__Rooms__HouseID]
+GO
+
+/****** Object:  Table [dbo].[Rooms]    Script Date: 27-06-2015 14:25:07 ******/
+DROP TABLE [dbo].[Rooms]
+GO
+
+
+/****** Object:  Table [dbo].[Rooms]    Script Date: 27-06-2015 14:25:07 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Rooms](
+	[RoomID] [int] IDENTITY(1,1) NOT NULL,
+	[HouseID] [int] NOT NULL,
+	[RoomNumber] [varchar](5) NOT NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[LastUpdatedOn] [datetime] NOT NULL,
+	[Status] [bit] NOT NULL,
+	[Monthlyrent] [varchar](20) NOT NULL,
+	[NoofBeds] [int] NOT NULL,
+	[Deposit] [varchar](20) NOT NULL,
+ CONSTRAINT [PK__Rooms] PRIMARY KEY CLUSTERED 
+(
+	[RoomID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+ALTER TABLE [dbo].[Rooms]  WITH CHECK ADD  CONSTRAINT [FK__Rooms__HouseID] FOREIGN KEY([HouseID])
+REFERENCES [dbo].[Houses] ([HouseID])
+GO
+
+ALTER TABLE [dbo].[Rooms] CHECK CONSTRAINT [FK__Rooms__HouseID]
+GO
+
+
+
---
+++ Database/2015/June/00008_Create_Beds_table.sql
@@ -0,0 +1,39 @@
+/****** Object:  Table [dbo].[Beds]    Script Date: 27-06-2015 16:07:56 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Beds](
+	[BedID] [int] IDENTITY(1,1) NOT NULL,
+	[RoomID] [int] NOT NULL,
+	[UserID] [int] NOT NULL,
+	[Status] [bit] NOT NULL,
+	[CreatedOn] [datetime] NULL,
+	[LastUpdatedOn] [datetime] NULL,
+	[Bedstatus] [int] NULL,
+	[Statusupdatedate] [datetime] NULL,
+ CONSTRAINT [PK_Bed] PRIMARY KEY CLUSTERED 
+(
+	[BedID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[Beds]  WITH CHECK ADD  CONSTRAINT [FK_Beds_RoomID] FOREIGN KEY([RoomID])
+REFERENCES [dbo].[Rooms] ([RoomID])
+GO
+
+ALTER TABLE [dbo].[Beds] CHECK CONSTRAINT [FK_Beds_RoomID]
+GO
+
+ALTER TABLE [dbo].[Beds]  WITH CHECK ADD  CONSTRAINT [FK_Beds_UserID] FOREIGN KEY([UserID])
+REFERENCES [dbo].[Users] ([UserID])
+GO
+
+ALTER TABLE [dbo].[Beds] CHECK CONSTRAINT [FK_Beds_UserID]
+GO
+
+
