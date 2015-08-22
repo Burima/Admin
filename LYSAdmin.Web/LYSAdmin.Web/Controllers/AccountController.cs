1fbad48a8fde5583086598c208a6d403416e60bf
ajaynbiswas <ajayn.biswas@gmail.com>
2015-08-23 01:13:20 +0530

    Image Crop - In progress

--- LYSAdmin.Web/LYSAdmin.Web/Controllers/AccountController.cs
+++ LYSAdmin.Web/LYSAdmin.Web/Controllers/AccountController.cs
@@ -6,6 +6,9 @@ using System.Web;
 using System.Web.Mvc;
 using LYSAdmin.Domain.UserManagement;
 using LYSAdmin.Web.Utilities;
+using System.Net;
+using System.Web.Routing;
+using LYSAdmin.Domain;
 
 namespace LYSAdmin.Web.Controllers
 {
@@ -114,6 +117,37 @@ namespace LYSAdmin.Web.Controllers
                 TempData["message"] = "Error in updating your profile.Please try again later";
             }
             return View(userViewModel);
+        }
+
+
+        [POST("Account/CropImage")]
+        public virtual ActionResult CropImage(string imagePath, decimal? cropPointX, decimal? cropPointY, decimal? imageCropWidth, decimal? imageCropHeight, string fileName)
+        {
+            if (string.IsNullOrEmpty(imagePath) || !cropPointX.HasValue || !cropPointY.HasValue || !imageCropWidth.HasValue || !imageCropHeight.HasValue)
+            {
+                return new HttpStatusCodeResult((int)HttpStatusCode.BadRequest);
+            }
+            byte[] imageBytes = null;
+            string[] imageUriPart = imagePath.Split(',');
+            string base64String = imageUriPart[1];
+            imageBytes = Convert.FromBase64String(base64String);
+            byte[] croppedImage = ImageHelper.CropImage(imageBytes, (int)cropPointX.Value, (int)cropPointY.Value, (int)imageCropWidth.Value, (int)imageCropHeight.Value);
+
+            if (!string.IsNullOrEmpty(fileName))
+            {
+                string[] getID = fileName.Split('_');
+                string tempFolderName = Server.MapPath("~/files/croppedImages/" + getID[0]);
+                DateTime timestamp = DateTime.Now;
+                string filename = getID[0] + String.Format("{0:d-M-yyyy HH-mm-ss}", timestamp);
+                FileHelper.SaveFile(croppedImage, tempFolderName, filename);
+
+                string photoPath = string.Concat("../files/croppedImages/", "/" + getID[0], "/" + filename + ".png");
+                return Json(new { PhotoPath = photoPath, filename = filename + ".png" }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return new HttpStatusCodeResult((int)HttpStatusCode.BadRequest);
+            }
         }
 
     }
--- LYSAdmin.Web/LYSAdmin.Web/Scripts/Custom/ViewProfile.js
+++ LYSAdmin.Web/LYSAdmin.Web/Scripts/Custom/ViewProfile.js
@@ -4,77 +4,90 @@
             if (message != "") {
                 $('#modalShowMessage').modal('show');
             }
-
-            var $image = $(".image-crop > img")
-            $($image).cropper({
-                aspectRatio: 1.618,
-                preview: ".img-preview",
-                done: function (data) {
-                    // Output the result data for cropping image.
+            $(".jcrop-holder").find("div").eq(0).addClass("formatDiv");
+           
+            $("#fileProfile").change(function (e) {
+                alert("hi");
+                var file = $(this).val();
+                var ext = file.split('.').pop();
+                if (ext.toLowerCase() == "gif" || ext.toLowerCase() == "jpeg" || ext.toLowerCase() == "jpg" || ext.toLowerCase() == "png") {
+                    readURL(this);
+                    $('#spnRemove').show();
+                }
+                else {
+                    fnDismissModal();
                 }
             });
+            //var $image = $(".image-crop > img")
+            //$($image).cropper({
+            //    aspectRatio: 1.618,
+            //    preview: ".img-preview",
+            //    done: function (data) {
+            //        // Output the result data for cropping image.
+            //    }
+            //});
 
-            var $inputImage = $("#inputImage");
-            if (window.FileReader) {
-                $inputImage.change(function () {
-                    var fileReader = new FileReader(),
-                            files = this.files,
-                            file;
+            //var $inputImage = $("#inputImage");
+            //if (window.FileReader) {
+            //    $inputImage.change(function () {
+            //        var fileReader = new FileReader(),
+            //                files = this.files,
+            //                file;
 
-                    if (!files.length) {
-                        return;
-                    }
+            //        if (!files.length) {
+            //            return;
+            //        }
 
-                    file = files[0];
+            //        file = files[0];
 
-                    if (/^image\/\w+$/.test(file.type)) {
-                        fileReader.readAsDataURL(file);
-                        fileReader.onload = function () {
-                            $inputImage.val("");
-                            $image.cropper("reset", true).cropper("replace", this.result);
-                        };
-                    } else {
-                        showMessage("Please choose an image file.");
-                    }
-                });
-            } else {
-                $inputImage.addClass("hide");
-            }
+            //        if (/^image\/\w+$/.test(file.type)) {
+            //            fileReader.readAsDataURL(file);
+            //            fileReader.onload = function () {
+            //                $inputImage.val("");
+            //                $image.cropper("reset", true).cropper("replace", this.result);
+            //            };
+            //        } else {
+            //            showMessage("Please choose an image file.");
+            //        }
+            //    });
+            //} else {
+            //    $inputImage.addClass("hide");
+            //}
 
-            $("#saveImage").click(function () {
-                var image_data = atob($image.cropper("getDataURL").split(',')[1]);
-                // Use typed arrays to convert the binary data to a Blob
-                var arraybuffer = new ArrayBuffer(image_data.length);
-                var view = new Uint8Array(arraybuffer);
-                for (var i = 0; i < image_data.length; i++) {
-                    view[i] = image_data.charCodeAt(i) & 0xff;
-                }
-                try {
-                    // This is the recommended method:
-                    var blob = new Blob([arraybuffer], { type: 'application/octet-stream' });
-                } catch (e) {
-                    // The BlobBuilder API has been deprecated in favour of Blob, but older
-                    // browsers don't know about the Blob constructor
-                    // IE10 also supports BlobBuilder, but since the `Blob` constructor
-                    //  also works, there's no need to add `MSBlobBuilder`.
-                    var bb = new (window.WebKitBlobBuilder || window.MozBlobBuilder);
-                    bb.append(arraybuffer);
-                    var blob = bb.getBlob('application/octet-stream'); // <-- Here's the Blob
-                }
+            //$("#saveImage").click(function () {
+            //    var image_data = atob($image.cropper("getDataURL").split(',')[1]);
+            //    // Use typed arrays to convert the binary data to a Blob
+            //    var arraybuffer = new ArrayBuffer(image_data.length);
+            //    var view = new Uint8Array(arraybuffer);
+            //    for (var i = 0; i < image_data.length; i++) {
+            //        view[i] = image_data.charCodeAt(i) & 0xff;
+            //    }
+            //    try {
+            //        // This is the recommended method:
+            //        var blob = new Blob([arraybuffer], { type: 'application/octet-stream' });
+            //    } catch (e) {
+            //        // The BlobBuilder API has been deprecated in favour of Blob, but older
+            //        // browsers don't know about the Blob constructor
+            //        // IE10 also supports BlobBuilder, but since the `Blob` constructor
+            //        //  also works, there's no need to add `MSBlobBuilder`.
+            //        var bb = new (window.WebKitBlobBuilder || window.MozBlobBuilder);
+            //        bb.append(arraybuffer);
+            //        var blob = bb.getBlob('application/octet-stream'); // <-- Here's the Blob
+            //    }
 
-                // Use the URL object to create a temporary URL
-                var url = (window.URL || window.webkitURL).createObjectURL(blob);
-                location.href = url; // <-- Download!
-                document.getElementById("profile").src = url;
-                // alert(url);
-                url = url.split(':')[1].replace('%3A',':').replace('%3A',':');
-                //alert(url);
-                alert("New URL" +url);
-                $('#profilePic').val(url);
-                //by default set width and height to 48px so the image size not increase
-                document.getElementById("profile").style.width = "48px";
-                document.getElementById("profile").style.height = "48px";
-            });
+            //    // Use the URL object to create a temporary URL
+            //    var url = (window.URL || window.webkitURL).createObjectURL(blob);
+            //    location.href = url; // <-- Download!
+            //    document.getElementById("profile").src = url;
+            //    // alert(url);
+            //    url = url.split(':')[1].replace('%3A',':').replace('%3A',':');
+            //    //alert(url);
+            //    alert("New URL" +url);
+            //    $('#profilePic').val(url);
+            //    //by default set width and height to 48px so the image size not increase
+            //    document.getElementById("profile").style.width = "48px";
+            //    document.getElementById("profile").style.height = "48px";
+            //});
 
             $("#changePassword").click(function () {
                 $("#modalChangePassword").modal({ backdrop: 'static', keyboard: false });
@@ -113,4 +126,73 @@ function fnSaveLocation() {
             hideProgress();
         }
     });
+}
+
+function fnRemove() {
+    $('#fileProfile').val("");
+    $('#spnRemove').hide();
+    //$('#imgProfile').attr('src', '../Content/Images/empty_logo.png');
+    $("#photoFilePath").html("No File Chosen");//
+}
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+
+            $("#divCropResizeImage").modal('show');
+            $("#hdnImageSource").val(e.target.result);
+
+            $('.jcrop-holder').replaceWith('');
+            $("#divCropResizeImage .modal-body").empty();
+            $("#divCropResizeImage .modal-body").append("<div class=\"row\" style=\"margin:5%;\">  <div class=\"col-lg-2\"></div><div class=\"col-lg-4\"><img id=\"demo3\" alt=\"Jcrop Example\" style=\"width: 100%\" /></div>");
+            //$("#divCropResizeImage .modal-body").append("<div class=\"col-md-6\"><div id=\"preview-pane\"><div class=\"preview-container\"><img id=\"imgPreview\"  class=\"jcrop-preview\" alt=\"Preview\" />");
+            $("#divCropResizeImage .modal-body").append("</div></div></div></div>");
+            $('#demo3').replaceWith('<img id="demo3" src="' + e.target.result + '"/>');
+            FormImageCrop.init();
+
+            $("#imgPreview").attr("src", e.target.result);
+            $("div.jcrop-holder img").attr("src", e.target.result);
+
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function fnLoadImage() {
+    $("#hdnIsImageCropped").val("1");
+    var imageCropWidth = $("#hdnImageCropWidth").val();
+    var imageCropHeight = $("#hdnImageCropHeight").val();
+    var cropPointX = $("#hdnCropPointX").val();
+    var cropPointY = $("#hdnCropPointY").val();
+
+    if (imageCropWidth == 0 && imageCropHeight == 0) {
+        alert("Please select crop area.");
+        return;
+    }
+
+    $.ajax({
+        url: '@Url.Action("CropImage","Account")',
+        type: 'POST',
+        data: {
+            imagePath: $("#demo3").attr("src"),
+            cropPointX: cropPointX,
+            cropPointY: cropPointY,
+            imageCropWidth: imageCropWidth,
+            imageCropHeight: imageCropHeight,
+            fileName: $("#hdnFileName").val()
+        },
+        success: function (data) {
+            $("#divCropResizeImage").modal('hide');
+            $("#fileProfile").files = data.PhotoPath;
+            $("#imgProfile").attr("src", data.PhotoPath);
+            $('#hdnFileName').val(data.filename);
+            $("#hdnIsImageCropped").val("1");
+        },
+        error: function (data) { }
+    });
+
+
+    //  $("#imgProfile").attr("src", $("#imgPreview").attr("src"));
+    // $("#spnRemove").show();
 }
