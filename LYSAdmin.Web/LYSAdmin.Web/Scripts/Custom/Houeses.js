ce6815ee11feb02b82a47fd3f20bc642ff192d20
ajaynbiswas <ajayn.biswas@gmail.com>
2016-01-10 00:41:41 +0530

    House Image insertion - In progress

--- LYSAdmin.Web/LYSAdmin.Web/Scripts/Custom/Houeses.js
+++ LYSAdmin.Web/LYSAdmin.Web/Scripts/Custom/Houeses.js
@@ -6,7 +6,7 @@ var InitialLatitude, InitialLongitude;
 var blocks;
 //converting all the areas in json list
 eval("var areaList = " + Areas);
-Dropzone.autoDiscover = false;
+
 $(document).ready(function () {
    
      //if area is not selected show modal
@@ -35,77 +35,56 @@ $(document).ready(function () {
         $('#divAddHouse').show();
      });
 
-   //dropzone
-//Dropzone.options.myAwesomeDropzone = {
-
-//    autoProcessQueue: false,
-//    parallelUploads: 100,
-//    maxFiles: 100,
-//    paramName: "files",
-    
-//    // Dropzone settings
-//    init: function () {
-//        var myDropzone = new Dropzone("#my-awesome-dropzone", { url: ImageUploadUrl })
-       
-//       this.on("sendingmultiple", function () {
-//        });
-//        this.on("successmultiple", function (files, response) {
-//        });
-//        this.on("errormultiple", function (files, response) {
-//        });
-//    }
-
-    //}
+  
 
     /***********************DropZone Manually creation*****************************/
+   
+        var previewNode = document.querySelector("#template");
+        previewNode.id = "";
+        var previewTemplate = previewNode.parentNode.innerHTML;
+        previewNode.parentNode.removeChild(previewNode);
+       
+        var myDropzone = new Dropzone(document.body, { // Make the whole body a dropzone
+            url: '~/Estate/HouseImageUpload', // Set the url
+            thumbnailWidth: 80,
+            thumbnailHeight: 80,
+            parallelUploads: 20,
+            autoDiscover : false,
+            previewTemplate: previewTemplate,
+            autoQueue: false, // Make sure the files aren't queued until manually added
+            previewsContainer: "#previews", // Define the container to display the previews
+            clickable: ".fileinput-button"
+        });
 
-    var previewNode = document.querySelector("#template");
-    var previewTemplate = previewNode.parentNode.innerHTML;
-    previewNode.parentNode.removeChild(previewNode);
+        myDropzone.on("addedfile", function (file) {
+            // Hookup the start button
+            $(".start").click(function(){
+                myDropzone.enqueueFile(file);
+            });
+               
+        });
 
-    var myDropzone = new Dropzone(document.body, { // Make the whole body a dropzone
-        url: '~/Estate/HouseImageUpload', // Set the url
-        thumbnailWidth: 80,
-        thumbnailHeight: 80,
-        parallelUploads: 20,
-        previewTemplate: previewTemplate,
-        autoQueue: false, // Make sure the files aren't queued until manually added
-        previewsContainer: "#previews", // Define the container to display the previews
-        clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
-    });
+        // Update the total progress bar
+        myDropzone.on("totaluploadprogress", function (progress) {
+            $("#total-progress .progress-bar").style.width = progress + "%";
+        });
 
-    myDropzone.on("addedfile", function (file) {
-        // Hookup the start button
-        file.previewElement.querySelector(".start").onclick = function () { myDropzone.enqueueFile(file); };
-    });
+        myDropzone.on("sending", function (file) {
+            // Show the total progress bar when upload starts
+           $("#total-progress").style.opacity = "1";
+            // And disable the start button
+            $(".start").setAttribute("disabled", "disabled");
+        });
 
-    // Update the total progress bar
-    myDropzone.on("totaluploadprogress", function (progress) {
-        document.querySelector("#total-progress .progress-bar").style.width = progress + "%";
-    });
+        // Hide the total progress bar when nothing's uploading anymore
+        myDropzone.on("queuecomplete", function (progress) {
+            $("#total-progress").style.opacity = "0";
+        });
 
-    myDropzone.on("sending", function (file) {
-        // Show the total progress bar when upload starts
-        document.querySelector("#total-progress").style.opacity = "1";
-        // And disable the start button
-        file.previewElement.querySelector(".start").setAttribute("disabled", "disabled");
-    });
+       
 
-    // Hide the total progress bar when nothing's uploading anymore
-    myDropzone.on("queuecomplete", function (progress) {
-        document.querySelector("#total-progress").style.opacity = "0";
-    });
-
-    // Setup the buttons for all transfers
-    // The "add files" button doesn't need to be setup because the config
-    // `clickable` has already been specified.
-    document.querySelector("#actions .start").onclick = function () {
-        myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
-    };
-    document.querySelector("#actions .cancel").onclick = function () {
-        myDropzone.removeAllFiles(true);
-    };
-
+    
+   
     /***********************DropZone Manually creation*****************************/
     //apartmet seletion change
     $("select[name='ApartmentID']").change(function () {
@@ -267,6 +246,15 @@ function fnOpenBasicAmenities() {
     $('#collapseBasicAmenities').addClass('in');
 }
 
+// Setup the buttons for all transfers
+// The "add files" button doesn't need to be setup because the config
+// `clickable` has already been specified.
+$(".start").click(function() {
+    myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
+});
+$(".cancel").click(function() {
+    myDropzone.removeAllFiles(true);
+});
 
 //show change location modal
 function fnChangeLocation() {
