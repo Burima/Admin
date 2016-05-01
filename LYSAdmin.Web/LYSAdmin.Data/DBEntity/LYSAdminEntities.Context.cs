8ec8098e72b6a2b2131f2babc80c4d47b1d39734
ajaynbiswas <ajayn.biswas@gmail.com>
2016-05-01 21:52:07 +0530

    edmx update

--- LYSAdmin.Web/LYSAdmin.Data/DBEntity/LYSAdminEntities.Context.cs
+++ LYSAdmin.Web/LYSAdmin.Data/DBEntity/LYSAdminEntities.Context.cs
@@ -22,7 +22,7 @@ namespace LYSAdmin.Data.DBEntity
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            throw new UnintentionalCodeFirstException();
+            
         }
     
         public virtual DbSet<Area> Areas { get; set; }
